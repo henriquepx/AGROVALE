f20477d6090e881bd0265f6bf7688d399e58a6a7
Henrique <henriquepinheiroxavier@gmail.com>
2024-10-26 08:33:07 -0300

    feat: change owner tasks

--- src/app/layout/Main.tsx
+++ src/app/layout/Main.tsx
@@ -5,7 +5,7 @@ const Main = () => {
     <main>
       <div className='main_size'>
         <div className='main_text'>
-          <h1>Agrovale Importação e Exportação</h1>
+          <h1>Agrovale<br /> Importação e Exportação</h1>
           <p>A escolha certa para a comercialização da sua pimenta-do-reino.</p>
           <a href="#" className='main_button'>Saiba mais</a>
         </div> 
